67d05b6f96140ac2f22b03e4d4be8bf611dbbd42
cristiansep <cristiansepulvedamendez@gmail.com>
2019-12-14 23:13:07 -0300

    Lista y funcionando

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: []
+})
+export class HomeComponent  {
+
+  nuevasCanciones: any[] = [];
+  loading: boolean;
+
+  error: boolean;
+  mensajeError: string;
+
+  constructor(private spotify: SpotifyService) {
+    this.loading = true;
+    this.error = false;
+    this.spotify.getNewReleases()
+                .subscribe((data: any) => {
+                    this.nuevasCanciones = data;
+                    this.loading = false;
+                }, (errorService) => {
+                    this.loading = false;
+                    this.error = true;
+                    this.mensajeError = errorService.error.error.message;
+                });
+  }
+
+
+
+}
